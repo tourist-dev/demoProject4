22a853e13b0bf27bece3f9df71fc5bd9880c51c5
mrsameer <sameershaik2001@outlook.com>
2021-05-24 13:02:55 +0530

    Fixed bug in AddServlet.java

--- src/main/java/com/example/demoProject4/AddServlet.java
+++ src/main/java/com/example/demoProject4/AddServlet.java
@@ -7,21 +7,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet
+@WebServlet(name="add-record", value="/add-record")
 public class AddServlet extends HttpServlet {
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
         out.print("<html><body>");
-        Insurance.records.add(request.getParameter("id" +
+        Insurance.records.add(request.getParameter("id" )+
                 ", " + request.getParameter("pnum") + ", " +
                 request.getParameter("peffdate") + ", " +
                 request.getParameter("pexpdate") + ", " +
                 request.getParameter("payment") + ", " +
                 request.getParameter("total") + ", " +
                 request.getParameter("active") + ", " +
-                request.getParameter("date")));
+                request.getParameter("date"));
         out.print("added");
         out.print("</body></html>");
     }
